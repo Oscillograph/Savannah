05a92f4abbe70fcf9f0a4d4010caff18fcc9bec8
Living Broscillograph <totservis@gmail.com>
2024-03-01 13:57:47 +0300

    improved interface in proforientator example

--- examples/proforientator.cpp
+++ examples/proforientator.cpp
@@ -1,5 +1,5 @@
 #include <include/savannah/savannah.h>
-#include "./proforientator/include/datatypes.h"
+// #include "./proforientator/include/datatypes.h"
 #include "./proforientator/include/yaml_wrapper.h"
 #include "./proforientator/include/logger.h"
 
@@ -46,6 +46,9 @@ namespace Savannah
 			static bool use_work_area = true;
 			// static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings;
 			static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoSavedSettings;
+			
+			const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
+			const float TEXT_BASE_HEIGHT = ImGui::GetTextLineHeightWithSpacing();
 			
 			// ====================================================================================
 			// The Application starts here
@@ -114,11 +117,154 @@ namespace Savannah
 						overlay, 
 						-1.0f, 
 						1.0f, 
-						ImVec2(600, 200.0f)
+						ImVec2(TEXT_BASE_WIDTH*60, TEXT_BASE_HEIGHT*10)
 						);
 				}
+				
+//				ImGui::ShowStackToolWindow();
+				
+				{
+					ImGui::Columns(4);
+					ImGui::SetColumnWidth(0, TEXT_BASE_WIDTH * 32);
+					ImGui::SetColumnWidth(1, TEXT_BASE_WIDTH * 32);
+					ImGui::SetColumnWidth(2, TEXT_BASE_WIDTH * 32);
+					ImGui::SetColumnWidth(3, TEXT_BASE_WIDTH * 32);
+					ImGui::TextColored({1.0f, 0.7f, 0.5f, 1.0f}, "Технологии");
+					if (ImGui::BeginTable("Технологии", 2, ImGuiTableFlags_ScrollY, {TEXT_BASE_WIDTH * 30, TEXT_BASE_HEIGHT * 10}))
+					{
+						// PushStyleCompact();
+						SkillGroup* group = (m_SkillsRegistry->GetGroups())["Технологии"];
+						for (auto skillsIterator = group->children.begin(); skillsIterator != group->children.end(); skillsIterator++)
+						{
+							std::string nameID = (*skillsIterator)->name;
+							ImGui::TableNextColumn();
+							ImGui::PushID(nameID.c_str());
+							ImGui::Text("%s", nameID.c_str());
+							ImGui::Spacing();
+							ImGui::TableNextColumn();
+							std::string valueID = (*skillsIterator)->name;
+							// ImGui::PushID(valueID.c_str());
+							std::string value = "";
+							for (int i = 0; i < 10; i++)
+							{
+								if (i < (*skillsIterator)->level)
+								{
+									value += "+";
+								} else {
+									value += " ";
+								}
+							}
+							ImGui::Text("%s", value.c_str());
+							ImGui::PopID();
+						}
+						// PopStyleCompact();
+						ImGui::EndTable();
+					}
+					
+					ImGui::NextColumn();
+					ImGui::TextColored({1.0f, 0.7f, 0.5f, 1.0f}, "Инструменты");
+					if (ImGui::BeginTable("Инструменты", 2, ImGuiTableFlags_ScrollY, {TEXT_BASE_WIDTH * 30, TEXT_BASE_HEIGHT * 10}))
+					{
+						// PushStyleCompact();
+						SkillGroup* group = (m_SkillsRegistry->GetGroups())["Инструменты"];
+						for (auto skillsIterator = group->children.begin(); skillsIterator != group->children.end(); skillsIterator++)
+						{
+							std::string nameID = (*skillsIterator)->name;
+							ImGui::TableNextColumn();
+							ImGui::PushID(nameID.c_str());
+							ImGui::Text("%s", nameID.c_str());
+							ImGui::Spacing();
+							ImGui::TableNextColumn();
+							std::string valueID = (*skillsIterator)->name;
+							// ImGui::PushID(valueID.c_str());
+							std::string value = "";
+							for (int i = 0; i < 10; i++)
+							{
+								if (i < (*skillsIterator)->level)
+								{
+									value += "+";
+								} else {
+									value += " ";
+								}
+							}
+							ImGui::Text("%s", value.c_str());
+							ImGui::PopID();
+						}
+						// PopStyleCompact();
+						ImGui::EndTable();
+					}
+					
+					ImGui::NextColumn();
+					ImGui::TextColored({1.0f, 0.7f, 0.5f, 1.0f}, "Запчасти");
+					if (ImGui::BeginTable("Запчасти", 2, ImGuiTableFlags_ScrollY, {TEXT_BASE_WIDTH * 30, TEXT_BASE_HEIGHT * 10}))
+					{
+						// PushStyleCompact();
+						SkillGroup* group = (m_SkillsRegistry->GetGroups())["Запчасти"];
+						for (auto skillsIterator = group->children.begin(); skillsIterator != group->children.end(); skillsIterator++)
+						{
+							std::string nameID = (*skillsIterator)->name;
+							ImGui::TableNextColumn();
+							ImGui::PushID(nameID.c_str());
+							ImGui::Text("%s", nameID.c_str());
+							ImGui::Spacing();
+							ImGui::TableNextColumn();
+							std::string valueID = (*skillsIterator)->name;
+							// ImGui::PushID(valueID.c_str());
+							std::string value = "";
+							for (int i = 0; i < 10; i++)
+							{
+								if (i < (*skillsIterator)->level)
+								{
+									value += "+";
+								} else {
+									value += " ";
+								}
+							}
+							ImGui::Text("%s", value.c_str());
+							ImGui::PopID();
+						}
+						// PopStyleCompact();
+						ImGui::EndTable();
+					}
+					
+					ImGui::NextColumn();
+					ImGui::TextColored({1.0f, 0.7f, 0.5f, 1.0f}, "Знания");
+					if (ImGui::BeginTable("Знания", 2, ImGuiTableFlags_ScrollY, {TEXT_BASE_WIDTH * 30, TEXT_BASE_HEIGHT * 10}))
+					{
+						// PushStyleCompact();
+						SkillGroup* group = (m_SkillsRegistry->GetGroups())["Знания"];
+						for (auto skillsIterator = group->children.begin(); skillsIterator != group->children.end(); skillsIterator++)
+						{
+							std::string nameID = (*skillsIterator)->name;
+							ImGui::TableNextColumn();
+							ImGui::PushID(nameID.c_str());
+							ImGui::Text("%s", nameID.c_str());
+							ImGui::Spacing();
+							ImGui::TableNextColumn();
+							std::string valueID = (*skillsIterator)->name;
+							// ImGui::PushID(valueID.c_str());
+							std::string value = "";
+							for (int i = 0; i < 10; i++)
+							{
+								if (i < (*skillsIterator)->level)
+								{
+									value += "+";
+								} else {
+									value += " ";
+								}
+							}
+							ImGui::Text("%s", value.c_str());
+							ImGui::PopID();
+						}
+						// PopStyleCompact();
+						ImGui::EndTable();
+					}
+					
+					ImGui::NextColumn();
+				}
+				
+				ImGui::End();
 			}
-			ImGui::End();
 			// ====================================================================================
 		}
 		
